b1876bafb93de937935399b61a362ff4b26013a8
MacieBorowi <leonardo.materhorn@gmail.com>
2016-01-26 08:41:28 +0100

    settingsInProject

--- spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
@@ -1,38 +1,39 @@
 package pl.spring.demo.service.impl;
 
+import java.util.List;
+
 import org.springframework.cache.annotation.Cacheable;
+
 import pl.spring.demo.dao.BookDao;
 import pl.spring.demo.service.BookService;
 import pl.spring.demo.to.BookTo;
 
-import java.util.List;
-
 public class BookServiceImpl implements BookService {
 
-    private BookDao bookDao;
+	private BookDao bookDao;
 
-    @Override
-    @Cacheable("booksCache")
-    public List<BookTo> findAllBooks() {
-        return bookDao.findAll();
-    }
+	@Override
+	@Cacheable("booksCache")
+	public List<BookTo> findAllBooks() {
+		return bookDao.findAll();
+	}
 
-    @Override
-    public List<BookTo> findBooksByTitle(String title) {
-        return bookDao.findBookByTitle(title);
-    }
+	@Override
+	public List<BookTo> findBooksByTitle(String title) {
+		return bookDao.findBookByTitle(title);
+	}
 
-    @Override
-    public List<BookTo> findBooksByAuthor(String author) {
-        return bookDao.findBooksByAuthor(author);
-    }
+	@Override
+	public List<BookTo> findBooksByAuthor(String author) {
+		return bookDao.findBooksByAuthor(author);
+	}
 
-    @Override
-    public BookTo saveBook(BookTo book) {
-        return bookDao.save(book);
-    }
+	@Override
+	public BookTo saveBook(BookTo book) {
+		return bookDao.save(book);
+	}
 
-    public void setBookDao(BookDao bookDao) {
-        this.bookDao = bookDao;
-    }
+	public void setBookDao(BookDao bookDao) {
+		this.bookDao = bookDao;
+	}
 }
